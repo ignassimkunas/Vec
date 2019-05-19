826f021ecb712f2774509a3f7a172bd448322941
ignassimkunas <ignassimkunas@gmail.com>
2019-05-19 18:29:13 +0300

    fixes

--- vector.h
+++ vector.h
@@ -19,17 +19,23 @@ public:
 	typedef const_value_type* const_pointer;
 	typedef ptrdiff_t difference_type;
 	typedef size_t size_type;
-	Vector() : sz(0), cpct(0), elem{allocator.allocate(cpct)} {} //default constructor
+	//constructs the vector
+	Vector() : sz(0), cpct(0), elem{allocator.allocate(cpct)} {}
+	//constructs the vector with given capacity
 	Vector(int size): sz(0),cpct(size), elem{allocator.allocate(size)}{}
-	Vector(const Vector& v): sz(v.sz), cpct(v.cpct), elem{allocator.allocate(v.cpct)}{ //copy constructor
+	//copy constructor
+	Vector(const Vector& v): sz(v.sz), cpct(v.cpct), elem{allocator.allocate(v.cpct)}{ 
 	  for (auto i=0; i!=v.sz; ++i)
 		elem[i] = v.elem[i];
 	}
+	//destructor
 	~Vector(){delete[] elem;}
+	//iterator class
 	class Iterator{
 	private:
 		pointer ptr;
 	public:
+		//assign value to iterator
 		void setPointer(pointer value){ptr = value;}
 		Iterator& operator=(const Iterator& it){ptr = it.ptr;}
 		Iterator& operator=(T value){*ptr = value;}
@@ -37,10 +43,16 @@ public:
 		reference operator*() const {return *ptr;}
 		pointer operator->() const {return ptr;}
 		Iterator operator++(int val){++ptr; return *this;}
+		Iterator operator++(){++ptr; return *this;}
 		Iterator operator--(int val){--ptr; return *this;}
 		Iterator operator+(int val){
 			for (auto i = 0; i < val; ++i)
 				++ptr;
+			return *this;
+		}
+		Iterator operator-(int val){
+			for (auto i = 0; i < val; ++i)
+				--ptr;
 			return *this;
 		}
 		bool operator==(Iterator a){return a.ptr == ptr;}
@@ -48,27 +60,41 @@ public:
 		bool operator==(pointer val){return val == ptr;}
 		bool operator!=(pointer val){return val != ptr;}
 	};
+	//const iterator
 	class ConstIterator{
 	private:
 		pointer ptr;
 	public:
+		//assigns value to const iterator
 		void setPointer(pointer value){ptr = value;}
 		ConstIterator& operator=(const ConstIterator& it) const{ptr = it.ptr;}
 		friend std::ostream& operator<<(std::ostream& out, const ConstIterator &it){out << it.ptr;}
-		reference operator*() const {return *ptr;}
-		pointer operator->() const {return ptr;}
+		const_reference operator*() const {return *ptr;}
+		const_pointer operator->() const {return ptr;}
 		ConstIterator operator++(int val){++ptr; return *this;}
 		ConstIterator operator--(int val){--ptr; return *this;}
+		ConstIterator operator+(int val){
+			for (auto i = 0; i < val; ++i)
+				++ptr;
+			return *this;
+		}
+		ConstIterator operator-(int val){
+			for (auto i = 0; i < val; ++i)
+				--ptr;
+			return *this;
+		}
 		bool operator==(ConstIterator a){return a.ptr == ptr;}
 		bool operator!=(ConstIterator a){return a.ptr != ptr;}
-		bool operator==(pointer val){return val == ptr;}
-		bool operator!=(pointer val){return val != ptr;}
+		bool operator==(const_pointer val){return val == ptr;}
+		bool operator!=(const_pointer val){return val != ptr;}
 		
 	};
+	//reverse iterator
 	class ReverseIterator{
 	private:
 		pointer ptr;
 	public:
+		//assigns value to reverse iterator
 		void setPointer(pointer value){ptr = value;}
 		ReverseIterator& operator=(const ReverseIterator& it){ptr = it.ptr;}
 		ReverseIterator& operator=(value_type value){*ptr = value;}
@@ -77,22 +103,44 @@ public:
 		pointer operator->() const {return ptr;}
 		ReverseIterator operator++(int val){--ptr; return *this;}
 		ReverseIterator operator--(int val){++ptr; return *this;}
+		ReverseIterator operator+(int val){
+			for (auto i = 0; i < val; ++i)
+				--ptr;
+			return *this;
+		}
+		ReverseIterator operator-(int val){
+			for (auto i = 0; i < val; ++i)
+				++ptr;
+			return *this;
+		}
 		bool operator==(ReverseIterator a){return a.ptr == ptr;}
 		bool operator!=(ReverseIterator a){return a.ptr != ptr;}
 		bool operator==(pointer val){return val == ptr;}
 		bool operator!=(pointer val){return val != ptr;}
 	};
+	//const reverse iterator
 	class ConstReverseIterator{
 	private:
 		pointer ptr;
 	public:
+		//assigns value to const reverse iterator
 		void setPointer(pointer value){ptr = value;}
 		ConstReverseIterator& operator=(const ConstReverseIterator& it)const {ptr = it.ptr;}
 		friend std::ostream& operator<<(std::ostream& out, const ConstReverseIterator &it){out << it.ptr;}
-		reference operator*() const {return *ptr;}
-		pointer operator->() const {return ptr;}
+		const_reference operator*() const {return *ptr;}
+		const_pointer operator->() const {return ptr;}
 		ConstReverseIterator operator++(int val){--ptr; return *this;}
 		ConstReverseIterator operator--(int val){++ptr; return *this;}
+		ConstReverseIterator operator+(int val){
+			for (auto i = 0; i < val; ++i)
+				--ptr;
+			return *this;
+		}
+		ConstReverseIterator operator-(int val){
+			for (auto i = 0; i < val; ++i)
+				++ptr;
+			return *this;
+		}
 		bool operator==(ConstReverseIterator a){return a.ptr == ptr;}
 		bool operator!=(ConstReverseIterator a){return a.ptr != ptr;}
 		bool operator==(pointer val){return val == ptr;}
@@ -103,18 +151,21 @@ public:
 	typedef ConstIterator const_iterator;
 	typedef ReverseIterator reverse_iterator;
 	typedef ConstReverseIterator const_reverse_iterator;
+	//assigns values to the container
 	Vector& operator=(const Vector& v){
 		sz = v.sz;
 		cpct= v.cpct;
 		if (cpct < v.cpct){elem = allocator.allocate(v.cpct);}
 		for (auto i = 0; i < v.sz; ++i){elem[i] = v.elem[i];}
 	}
+	//assigns values to the container by given value and index
 	void assign(int num, value_type value){
 		for(auto i = 0; i < num; ++i){
 			elem[i] = value;
 			sz++;
 		}
 	}
+	//assigns values to the container by given iterator range
 	void assign(iterator element, iterator lastElem){
 		size_type count = 0;
 		for(auto i = element; i != lastElem; i++){
@@ -123,32 +174,40 @@ public:
 			sz++;
 		}
 	}
+	//returns the associated allocator
 	allocator_type get_allocator(){
 		return allocator;
 	}
+	//access specified element
 	value_type operator[](size_type ind){
 		if (ind >= sz) {throw std::out_of_range("Out of scope");}
 		else {return elem[ind];}
 	}
+	//access specified element
 	reference at(size_type index){
 		if (index < sz){return elem[index];}
 		else {throw std::out_of_range("Out of scope");}
 	}
+	//access first element
 	reference front(){return elem[0];}
+	//access last element
 	reference back(){return elem[sz-1];}
+	//direct access to the underlying array
 	pointer data(){return elem;}
+	//returns an iterator to the beginning
 	iterator begin(){
 		iterator it;
 		it.setPointer(&elem[0]);
 		return it;
 	}
+	//returns an iterator to the end
 	iterator end(){
 		iterator it;
 		if(sz == 0){it.setPointer(&elem[0]);}
-		else {it.setPointer(&elem[sz-1]);}
-		
+		else {it.setPointer(&elem[sz]);}
 		return it;
 	}
+	//returns an iterator to the begining
 	const_iterator cbegin(){
 		const_iterator it;
 		it.setPointer(&elem[0]);
@@ -157,13 +216,13 @@ public:
 	const_iterator cend(){
 		const_iterator it;
 		if(sz == 0){it.setPointer(&elem[0]);}
-		else {it.setPointer(&elem[sz-1]);}
+		else {it.setPointer(&elem[sz]);}
 		return it;
 	}
 	reverse_iterator rbegin(){
 		reverse_iterator it;
-		if(sz == 0){it.setPointer(&elem[sz - 1]);}
-		else {it.setPointer(&elem[0]);}
+		if(sz == 0){it.setPointer(&elem[0]);}
+		else {it.setPointer(&elem[sz]);}
 		return it;
 	}
 	reverse_iterator rend(){
@@ -173,8 +232,8 @@ public:
 	}
 	const_reverse_iterator crbegin(){
 		const_reverse_iterator it;
-		if(sz == 0){it.setPointer(&elem[sz - 1]);}
-		else {it.setPointer(&elem[0]);}
+		if(sz == 0){it.setPointer(&elem[0]);}
+		else {it.setPointer(&elem[sz]);}
 		return it;
 	}
 	const_reverse_iterator crend(){
@@ -281,7 +340,15 @@ public:
 			sz++;
 		}
 	}
-	void pop_back(){sz--;}
+	void pop_back(){
+		if (sz > 0){
+			pointer temp = allocator.allocate(sz - 1);
+			std::copy(elem, elem + sz - 1, temp);
+			delete[] elem;
+			elem = temp;
+			sz--;
+		}
+	}
 	void resize(size_type new_sz){
 		if (sz < new_sz){
 			pointer temp = allocator.allocate(new_sz);
@@ -329,8 +396,8 @@ public:
 		}
 	}
 	friend bool operator<=(Vector& v, Vector& t){if (v == t){return true;}else {return v < t;}}
-	friend bool operator >(Vector& v, Vector& t){return !(v < t);}
-	friend bool operator >=(Vector& v, Vector& t){if (v == t){return true;}else {return !(v < t);}}
+	friend bool operator>(Vector& v, Vector& t){return !(v < t);}
+	friend bool operator>=(Vector& v, Vector& t){if (v == t){return true;}else {return !(v < t);}}
 };
 
 
